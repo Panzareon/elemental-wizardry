8c94c916fee6cfc037aac742f7b90f7d29c47f7e
Panzareon <fogtociag@web.de>
2024-01-19 18:46:40 +0100

    Add button to attune item

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Item } from '../model/item';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-item',
@@ -7,6 +8,14 @@ import { Item } from '../model/item';
   styleUrls: ['./item.component.less']
 })
 export class ItemComponent {
-
+  public constructor(private _data : DataService) {
+  }
   @Input() item!: Item;
+
+  public get canAttune() : boolean {
+    return !this._data.wizard.attunedItems.includes(this.item);
+  }
+  public attune() {
+    this._data.wizard.attuneItem(this.item);
+  }  
 }
